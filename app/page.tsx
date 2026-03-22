1fdf2d05d406e7c7d7488d674aabb10359f0a964
quintas y granjas <inmoflow25@gmail.com>
2026-03-22 14:57:24 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,7 @@ export default function Home() {
       return
     }
 
-    alert("Revisá tu email para ingresar 🚀")
+    alert("Te anviamos un enlace por mail para ingresar. Por favor reivsalo y dale click🚀")
     setShowLogin(false)
   }
 
