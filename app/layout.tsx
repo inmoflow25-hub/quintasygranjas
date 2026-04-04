0b35af16b3d909bd11f1196ed877679f7b2d26d5
quintas y granjas <inmoflow25@gmail.com>
2026-04-04 10:39:55 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Playfair_Display } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import Script from "next/script"
 import './globals.css'
+import "leaflet/dist/leaflet.css"
 
 const playfair = Playfair_Display({
   subsets: ["latin"],
