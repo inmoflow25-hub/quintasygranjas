07dcfb866ed40ea624a65685e7b54f498a1a6e2a
quintas y granjas <inmoflow25@gmail.com>
2026-04-12 16:44:30 -0300

    Update cart.tsx

--- components/cart/cart.tsx
+++ components/cart/cart.tsx
@@ -45,7 +45,7 @@ const PRODUCTS: Product[] = [
   // -------------------
   // POLLO
   // -------------------
-  { id: "pollo_entero", name: "Pollo entero", price: 18500, type: "weight_1kg", image: "https://pub-6d50e72dcfe845d5b97f24b5ac57f161.r2.dev/pollo%20entero.jpg", category: "pollo" },
+  { id: "pollo_entero", name: "Pollo entero", price: 18500, type: "unit", image: "https://pub-6d50e72dcfe845d5b97f24b5ac57f161.r2.dev/pollo%20entero.jpg", category: "pollo" },
   { id: "suprema", name: "Suprema de pollo", price: 11300, type: "weight_1kg", image: "https://pub-6d50e72dcfe845d5b97f24b5ac57f161.r2.dev/pechugas%20.jpg", category: "pollo" },
   { id: "muslos", name: "Muslos de pollo", price: 0, type: "weight_1kg", image: "https://pub-6d50e72dcfe845d5b97f24b5ac57f161.r2.dev/muslos%20d%20epolllo.jpg", category: "pollo" },
 {
