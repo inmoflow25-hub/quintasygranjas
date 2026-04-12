44d8675d58921953fe07a9127b6384d750819e19
quintas y granjas <inmoflow25@gmail.com>
2026-04-12 16:56:11 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,9 @@ export default function Home() {
       <Header />
       <Hero onWhatsAppClick={onWhatsAppClick} />
       <HowItWorks />
+      <div id="cart" className="scroll-mt-32">
       <Cart />
+      </div>
       <BoxesSection onSelectBox={onSelectBox} />
       <SocialProof />
       <DeliveryZones />
