c9bf62d46411f4950cd6abc175593786bb6dd4e9
quintas y granjas <inmoflow25@gmail.com>
2026-04-13 22:21:45 -0300

    Create boxes.ts

---
+++ lib/boxes.ts
@@ -0,0 +1,20 @@
+export const BOX_CATALOG: Record<
+  string,
+  {
+    name: string
+    price: number
+  }
+> = {
+  "dff394c8-6a17-45e8-ba3f-960c27f8d76c": {
+    name: "Caja Veggie",
+    price: 19530
+  },
+  "d9c75e5b-3e8b-4d3d-9776-d65ad9afae1d": {
+    name: "Caja Campo",
+    price: 42780
+  },
+  "d5b70577-a2b7-47d7-9ccd-e2f336e25af7": {
+    name: "Caja Granja",
+    price: 52980
+  }
+}
