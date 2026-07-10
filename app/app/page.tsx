b8e9c0e1736c7157a0b11c4ae24127de2cbf6411
quintas y granjas <inmoflow25@gmail.com>
2026-07-10 16:17:44 -0300

    Update page.tsx

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useMemo, useState } from "react"
 import { useRouter } from "next/navigation"
+import InstallAppButton from "@/components/pwa/install-app-button"
 
 type Product = {
   id: string
