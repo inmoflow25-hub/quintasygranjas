66971bc515249a59d0f5d94e240e6d9490452184
quintas y granjas <inmoflow25@gmail.com>
2026-07-20 18:07:29 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -78,14 +78,14 @@ self.addEventListener("push", (event) => {
   }
 
   event.waitUntil(
-    self.registration.showNotification(data.title, {
-      body: data.body,
-      icon: "/icons/icon-192.png",
-      badge: "/icons/icon-192.png",
-      data: {
-        url: data.url || "/app"
-      }
-    })
+   self.registration.showNotification(data.title, {
+  body: data.body,
+  icon: "/icons/icon-192.png",
+  badge: "/icons/icon-192.png",
+  data: {
+    url: data.url || "/app"
+  }
+})
   )
 })
 
