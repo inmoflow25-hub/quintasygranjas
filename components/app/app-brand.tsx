4ddc8414e3ba49088612a158e4fda609701799ee
quintas y granjas <inmoflow25@gmail.com>
2026-07-23 10:05:02 -0300

    Create app-brand.tsx

---
+++ components/app/app-brand.tsx
@@ -0,0 +1,17 @@
+export default function AppBrand() {
+  return (
+    <a href="/app" className="flex items-center gap-3">
+      <img
+        src="/logho quintas nuevo.svg"
+        alt=""
+        className="h-10 w-10 object-contain"
+      />
+
+      <img
+        src="/brand/qyg-logo.svg"
+        alt="Quintas y Granjas"
+        className="h-14 w-auto"
+      />
+    </a>
+  )
+}
