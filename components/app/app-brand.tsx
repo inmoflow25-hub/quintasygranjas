01899081d2d29c104779a053a8a417da0d150801
quintas y granjas <inmoflow25@gmail.com>
2026-07-24 16:34:13 -0300

    Update app-brand.tsx

--- components/app/app-brand.tsx
+++ components/app/app-brand.tsx
@@ -1,28 +1,20 @@
 type AppBrandProps = {
   href?: string
   className?: string
-  iconClassName?: string
   logoClassName?: string
 }
 
 export default function AppBrand({
   href = "/",
   className = "",
-  iconClassName = "h-10 w-10",
-  logoClassName = "h-14 w-auto"
+  logoClassName = "h-16 w-auto"
 }: AppBrandProps) {
   return (
-    <a href={href} className={`flex items-center gap-3 ${className}`}>
+    <a href={href} className={`inline-flex items-center ${className}`}>
       <img
-        src="/logho quintas nuevo.svg"
-        alt=""
-        className={`${iconClassName} object-contain`}
-      />
-
-      <img
-        src="/brand/qyg-logo.svg"
+        src="/brand/qyg-isologotipo.png"
         alt="Quintas y Granjas"
-        className={logoClassName}
+        className={`${logoClassName} object-contain`}
       />
     </a>
   )
